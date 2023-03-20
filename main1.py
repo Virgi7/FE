328ecd21cd27791e1c7615a26aa626a0865b58c2
Virgi7 <128376370+Virgi7@users.noreply.github.com>
2023-03-20 17:52:31 +0100

    ciao

--- main1.py
+++ main1.py
@@ -8,17 +8,7 @@ import math
 import utilities as ut
 
 df=pd.read_csv('EUROSTOXX50_2023_Dataset.csv', index_col=0)
-df=df.fillna(method='ffill') #we fill the missing values of the stocks with the previous known ones
-
-#We select stocks properly in order to perform the right computation of the returns
-df_ptf=df[824:1592] #we selected 3y from 20-03-2019 bckw. up to 22-03-2016
-df_ptf=df_ptf.loc[:,['ADSGn.DE','ALVG.DE','MUVGn.DE','OREP.PA']] #we select only the 4 columns corresponding to Adidas, Allianz, Munich RE and l'Oreal
-df_den=df[825:1593] #we selected 3y from one day before 20-03-2019 (19-03-2019) up one day before the last date of df_ptf (21-03-2016)
-df_den=df_den.loc[:,['ADSGn.DE','ALVG.DE','MUVGn.DE','OREP.PA']] 
-
-#we pass to numpy arrays to perform the logarithm
-np_den=df_den.to_numpy() 
-np_num=df_ptf.to_numpy()
+(np_num, np_den)=ut.read_our_CSV(df)
 
 #Parameters
 alpha=0.95
--- utilities.py
+++ utilities.py
@@ -17,6 +17,21 @@ def AnalyticalNormalMeasures(alpha, weights, portfolioValue, riskMeasureTimeInte
     ES_std=norm.pdf(VaR_std)/(1-alpha) #ES for a std normal is the pdf of a normal evaluated in the VaR of a std normal
     ES=riskMeasureTimeIntervalInDay*Loss_mean+math.sqrt(riskMeasureTimeIntervalInDay)*Loss_std*ES_std #ES expressed in function of ES_std
     return VaR, ES
+def read_our_CSV(df):
 
+    df = df.fillna(method='ffill')  # we fill the missing values of the stocks with the previous known ones
+
+    # We select stocks properly in order to perform the right computation of the returns
+    df_ptf = df[824:1592]  # we selected 3y from 20-03-2019 bckw. up to 22-03-2016
+    df_ptf = df_ptf.loc[:, ['ADSGn.DE', 'ALVG.DE', 'MUVGn.DE',
+                            'OREP.PA']]  # we select only the 4 columns corresponding to Adidas, Allianz, Munich RE and l'Oreal
+    df_den = df[
+             825:1593]  # we selected 3y from one day before 20-03-2019 (19-03-2019) up one day before the last date of df_ptf (21-03-2016)
+    df_den = df_den.loc[:, ['ADSGn.DE', 'ALVG.DE', 'MUVGn.DE', 'OREP.PA']]
+
+    # we pass to numpy arrays to perform the logarithm
+    np_den = df_den.to_numpy()
+    np_num = df_ptf.to_numpy()
+    return np_num, np_den
 
 

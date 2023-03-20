ce398f0b19cadb2511a0b17618406bc0c4284330
M1ch34 <michelemassaria2000@gmail.com>
2023-03-20 18:15:08 +0100

    exercise 1.utilities

--- utilities_2.py
+++ utilities_2.py
@@ -9,7 +9,7 @@ def HSMeasurements(returns, alpha, weights, portfolioValue, RiskMeasureTimeInter
     nsamples = int(returns.shape[0] / RiskMeasureTimeIntervalInDay)
     addedreturns = np.zeros((nsamples, returns.shape[1]))
     for i in range(nsamples):
-        for j in range(i * RiskMeasureTimeIntervalInDay, ((i+1) * RiskMeasureTimeIntervalInDay)):
+        for j in range(i * RiskMeasureTimeIntervalInDay, ((i + 1) * RiskMeasureTimeIntervalInDay)):
             addedreturns[i, :] = addedreturns[i, :] + returns[j, :]
     loss = -portfolioValue * addedreturns.dot(weights)
     loss_sorted = sorted(loss)
@@ -41,11 +41,13 @@ def WHSMeasurements(returns, alpha, llambda, weights, portfolioValue, RiskMeasur
     i += -1
     print(i)
     VaR = loss_sorted[i]
-    ES = (sum(loss_sorted[1:i] * lambdas_sorted[1:i]) + loss_sorted[0] * lambdas_sorted[0]) / (sum(lambdas_sorted[1:i]) + lambdas[0])
-    return[ES, VaR]
+    ES = (sum(loss_sorted[1:i] * lambdas_sorted[1:i]) + loss_sorted[0] * lambdas_sorted[0]) / (
+                sum(lambdas_sorted[1:i]) + lambdas[0])
+    return [ES, VaR]
 
 
-def PrincCompAnalysis(yearlyCovariance, yearlyMeanReturns, weights, H, alpha, numberOfPrincipalComponents, portfolioValue):
+def PrincCompAnalysis(yearlyCovariance, yearlyMeanReturns, weights, H, alpha, numberOfPrincipalComponents,
+                      portfolioValue):
     eigenvalues, eigenvectors = linalg.eigvals(yearlyCovariance)
     gamma = np.zeros((len(eigenvalues), len(eigenvalues)))
     eigenvalues_sorted = sorted(eigenvalues)
@@ -57,17 +59,18 @@ def PrincCompAnalysis(yearlyCovariance, yearlyMeanReturns, weights, H, alpha, nu
         mean_sorted[:, i] = yearlyMeanReturns[eigenvalues == eigenvalues_sorted[i]]
     weights_hat = gamma.T.dot(weights_sorted)
     mean_hat = gamma.T.dot(mean_sorted)
-    sigma_red = (H*sum(eigenvalues_sorted[0:numberOfPrincipalComponents]*weights_hat[0:numberOfPrincipalComponents]**2))**(1/2)
-    mean_red = H*sum(mean_hat[0:numberOfPrincipalComponents]*weights_hat[0:numberOfPrincipalComponents]**2)
-    VaR = portfolioValue*(mean_red + sigma_red*st.norm.ppf(alpha))
-    ES = portfolioValue*(mean_red + sigma_red*st.norm.pdf(st.norm.ppf(alpha))/(1 - alpha))
-    return[ES, VaR]
+    sigma_red = (H * sum(
+        eigenvalues_sorted[0:numberOfPrincipalComponents] * weights_hat[0:numberOfPrincipalComponents] ** 2)) ** (1 / 2)
+    mean_red = H * sum(mean_hat[0:numberOfPrincipalComponents] * weights_hat[0:numberOfPrincipalComponents] ** 2)
+    VaR = portfolioValue * (mean_red + sigma_red * st.norm.ppf(alpha))
+    ES = portfolioValue * (mean_red + sigma_red * st.norm.pdf(st.norm.ppf(alpha)) / (1 - alpha))
+    return [ES, VaR]
 
 
 def bootstrapStatistical(numberOfSamplesToBootstrap, returns, alpha, portfolioValue, RiskMeasureTimeIntervalInDay):
     n = returns.shape[1]
     samples = np.zeros(numberOfSamplesToBootstrap, 1)
     for i in range(numberOfSamplesToBootstrap):
-        x = int(random.randint(0,n-1))
+        x = int(random.randint(0, n - 1))
         samples[i] = HSMeasurements(returns[:, x], alpha, 1, portfolioValue, RiskMeasureTimeIntervalInDay)
     return samples

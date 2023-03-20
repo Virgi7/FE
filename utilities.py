b5cb531d24b88934b955d75fd9037aa72314534c
Virgi7 <128376370+Virgi7@users.noreply.github.com>
2023-03-20 16:51:38 +0100

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,1 +1,21 @@
-print('Hello')
+#Funzioni Assigment 5.0
+import pandas as pd
+import numpy as np
+import scipy
+import math
+from scipy.stats import norm
+
+
+
+def AnalyticalNormalMeasures(alpha, weights, portfolioValue, riskMeasureTimeIntervalInDay,returns):
+    Loss=-portfolioValue*(returns*weights) #moltiplico per i pesi perchè returns è una matrice num_datexnum_assets e weights è n_assetx1
+    Loss_mean=np.mean(Loss)
+    Loss_std=np.std(Loss)
+    VaR_std=norm.ppf(alpha)
+    VaR=riskMeasureTimeIntervalInDay*Loss_mean+math.sqrt(riskMeasureTimeIntervalInDay)*Loss_std*VaR_std
+    ES_std=norm.pdf(VaR_std)
+    ES=riskMeasureTimeIntervalInDay*Loss_mean+math.sqrt(riskMeasureTimeIntervalInDay)*Loss_std*ES_std
+    return VaR, ES
+
+
+

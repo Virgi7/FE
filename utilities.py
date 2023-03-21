cd6e7ea0b7c166941f24ed35a32db9d188f1a4c0
TiziMarr99 <tiziano.marrone@mail.polimi.it>
2023-03-21 15:01:38 +0100

    che palle

--- utilities.py
+++ utilities.py
@@ -26,11 +26,11 @@ def read_our_CSV(df):
     df = df.fillna(method='ffill')  # we fill the missing values of the stocks with the previous known ones
 
     # We select stocks properly in order to perform the right computation of the returns
-    df_ptf = df[824:1592]  # we selected 3y from 20-03-2019 bckw. up to 22-03-2016
+    df_ptf = df['2016-03-22':'2019-03-20']  # we selected 3y from 20-03-2019 bckw. up to 22-03-2016
     df_ptf = df_ptf.loc[:, ['ADSGn.DE', 'ALVG.DE', 'MUVGn.DE',
                             'OREP.PA']]  # we select only the 4 columns corresponding to Adidas, Allianz, Munich RE
     # and l'Oreal
-    df_den = df[825:1593]  # we selected 3y from one day before 20-03-2019 (19-03-2019) up one day before the last date
+    df_den = df['2016-03-21':'2019-03-19']  # we selected 3y from one day before 20-03-2019 (19-03-2019) up one day before the last date
     # of df_ptf (21-03-2016)
     df_den = df_den.loc[:, ['ADSGn.DE', 'ALVG.DE', 'MUVGn.DE', 'OREP.PA']]
 

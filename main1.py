da83e973cfcc57b462a8f8c662424b6f2c48b3b6
Virgi7 <128376370+Virgi7@users.noreply.github.com>
2023-03-21 15:02:10 +0100

    l

--- main1.py
+++ main1.py
@@ -6,9 +6,11 @@ import scipy as sc
 from scipy.stats import norm
 import math
 import utilities as ut
+import control as co
 
 df=pd.read_csv('EUROSTOXX50_2023_Dataset.csv', index_col=0)
 (np_num, np_den)=ut.read_our_CSV(df)
+
 
 #Parameters
 alpha=0.95
@@ -18,6 +20,12 @@ n_asset=4
 weights=np.ones((n_asset,1))/n_asset #we consider a equally weighted portfolio
 
 returns=np.log(np_num/np_den) #computation of the returns
+print(np.shape(returns[:,1]))
+C=np.corrcoef(returns.T)
+print(np.shape(C))
+#print(np.shape(returns))
 
 VaR, ES=ut.AnalyticalNormalMeasures(alpha, weights, notional, delta, returns) #we call the implemented function to calculate VaR and ES
 print(VaR, ES)
+VaR_check=co.plausibilityCheck(returns, weights, alpha, notional, delta)
+print(VaR_check)

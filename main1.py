6ec7b2a3fe46e0e7457bfd0ea6938a8dcc82c477
Virgi7 <128376370+Virgi7@users.noreply.github.com>
2023-03-22 13:50:51 +0100

    v

--- main1.py
+++ main1.py
@@ -3,10 +3,6 @@ import pandas as pd
 import numpy as np
 import utilities as ut
 
-import control as co
-
-import utilities_2 as ut2
-
 
 df = pd.read_csv('EUROSTOXX50_2023_Dataset.csv', index_col=0)
 (np_num, np_den) = ut.read_our_CSV(df)
@@ -26,21 +22,7 @@ print(np.shape(C))
 #print(np.shape(returns))
 VaR, ES = ut.AnalyticalNormalMeasures(alpha,weights,notional,delta,returns)
 print(VaR, ES)
-VaR_check=co.plausibilityCheck(returns, weights, alpha, notional, delta)
+VaR_check=ut.plausibilityCheck(returns, weights, alpha, notional, delta)
 print(VaR_check)
 
-# Parameters
-alpha = 0.95
-notional = 1e7
-delta = 3
-n_asset = 4
-weights = np.ones((n_asset, 1))/n_asset  # we consider a equally weighted portfolio
-
-returns = np.log(np_num/np_den)  # computation of the returns
-
-# we call the implemented function to calculate VaR and ES
-VaR, ES = ut2.HSMeasurements(returns, alpha, weights, notional, delta)
-print(VaR, ES)
-VaR_check=co.plausibilityCheck(returns, weights, alpha, notional, delta)
-print(VaR_check)
 

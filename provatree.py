76b08e9a32a13d79db5d34a0e30ee86eb909c81b
Virgi7 <128376370+Virgi7@users.noreply.github.com>
2023-03-24 15:37:45 +0100

    l

--- provatree.py
+++ provatree.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import math
 import option as opt
+from datetime import datetime
+from datetime import date
+
 
 
 
@@ -12,4 +15,24 @@ S0=1
 delta=1
 T=4
 tree1=opt.tree_gen(sigma, steps, S0, delta,T)
-print(tree1)
+
+disc = pd.read_excel('dat_disc.xlsx')
+disc.to_numpy()
+print(disc)
+
+
+dates=['2025-02-02','2026-02-02','2027-02-02','2028-02-02']
+#sett_date=datetime.strptime(sett_date,"%Y-%m-%d")
+#print(sett_date)
+
+
+
+sett_date_y=np.zeros((4,1))
+for i in range(len(dates)):
+    dates[i] = datetime.strptime(dates[i], "%Y-%m-%d")
+    sett_date_y[i]=date.toordinal(dates[i])
+print(sett_date_y)
+
+
+
+

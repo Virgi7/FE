2e4300f047e01a3cd1b87939c8b3129e6576825d
M1ch34 <michelemassaria2000@gmail.com>
2023-03-25 18:50:13 +0100

    modifiche tree

--- option.py
+++ option.py
@@ -1,10 +1,9 @@
 import numpy as np
-import pandas as pd
 import math
 
 
-def tree_gen(sigma, steps, S0, delta, T):#T è la maturity
-    u=math.exp(sigma*math.sqrt(delta/steps)) # Delta = 1 anno/n = numero di step per ogni anno
+def tree_gen(sigma, steps, S0, delta, T):# T è la maturity
+    u=math.exp(sigma*math.sqrt(delta/steps)) # Delta = 1 year/n = number of steps for each year
     d=math.exp(-sigma*math.sqrt(delta/steps))
     q=(1 - d) / (u - d)
     tree=np.zeros((int(steps*T/delta+1), int(steps*T/delta+1)))
@@ -12,22 +11,26 @@ def tree_gen(sigma, steps, S0, delta, T):#T è la maturity
     for i in range(int(steps*T/delta)):
         for j in range(int(steps*T/delta)):
             tree[i+1][j+1] = tree[i][j]*d
-        for j in range(i+1, int(steps*T/delta+1),1):
+        for j in range(i+1, int(steps*T/delta+1), 1):
             tree[i][j] = tree[i][j-1]*u
-    return tree[:, range(steps, steps*T+1, steps)],q
+    return tree[:, range(steps, steps*T+1, steps)], q
 
 
 def priceCliquet(S0, disc, tree, n, q, rec, SurProbFun, datesInYears):
-    # Survival probabilities for the expires in datesInYears (T=0 is included)
-    survProb = [SurProbFun(T) for T in datesInYears]
-    defProb = [SurProbFun(T-1)-SurProbFun(T) for T in datesInYears]
+    # Survival probabilities for the expires in datesInYears
+    survProb = np.array([SurProbFun(T) for T in datesInYears])
+    defProb = np.array([(SurProbFun(T-1)-SurProbFun(T))*rec for T in datesInYears])
     T = datesInYears[len(datesInYears)-1]
-    payoff = tree
-    for i in range(T-1):
-        for j in range((i+1)*n):
-            for k in range(j, j+n+1):
-                payoff[i+1, j] = (tree[i+1, k] - tree[i+1, j] * (i > 0) - S0 * (i == 0)) * q ** ((i+1)*n - k) * (1 - q) ** k
-    payoff = payoff * (disc * (survProb + rec * defProb))
+    payoff = np.zeros(tree.shape)
+    for i in range(T):
+        if i == 0:
+            for j in range(n):
+                payoff[j, i] += np.max([(tree[j, i] - S0), 0]) * q ** ((i + 1) * n - j) * (1 - q) ** j
+        else:
+            for j in range(i*n):
+                for k in range(j, j + n + 1):
+                    payoff[k, i] += np.max([(tree[k, i] - tree[j, i-1]), 0]) * q ** ((i + 1) * n - k) * (1 - q) ** k
+    payoff = payoff * disc * (survProb + defProb)
     price = sum(sum(payoff))
     return price
 
--- provatree.py
+++ provatree.py
@@ -3,13 +3,16 @@ import pandas as pd
 import math
 import option as opt
 
+sigma = math.sqrt(0.25)
+steps = 24
+S0 = 1
+delta = 1
+T = 4
+tree, q = opt.tree_gen(sigma, steps, S0, delta, T)
 
 
+def SurProbFun(t):
+    return np.exp(4 * t * 10 ** (-4))
 
-sigma=math.sqrt(0.25)
-steps=3
-S0=1
-delta=1
-T=4
-tree1=opt.tree_gen(sigma, steps, S0, delta,T)
-print(tree1)
+
+price = opt.priceCliquet(S0, [0.94, 0.9, 0.88, 0.87], tree, steps * T, q, 0.3, SurProbFun, [1, 2, 3, 4])

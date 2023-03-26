332af2ab357f351d27cc4110b6aa254f2d7cb3f8
M1ch34 <michelemassaria2000@gmail.com>
2023-03-26 16:35:07 +0200

    albero modifiche

--- option.py
+++ option.py
@@ -1,36 +1,51 @@
 import numpy as np
 import math
+import scipy.stats as st
 
 
 def tree_gen(sigma, steps, S0, delta, T):# T è la maturity
-    u=math.exp(sigma*math.sqrt(delta/steps)) # Delta = 1 year/n = number of steps for each year
-    d=math.exp(-sigma*math.sqrt(delta/steps))
-    q=(1 - d) / (u - d)
-    tree=np.zeros((int(steps*T/delta+1), int(steps*T/delta+1)))
-    tree[0][0]=S0
-    for i in range(int(steps*T/delta)):
-        for j in range(int(steps*T/delta)):
-            tree[i+1][j+1] = tree[i][j]*d
-        for j in range(i+1, int(steps*T/delta+1), 1):
-            tree[i][j] = tree[i][j-1]*u
-    return tree[:, range(steps, steps*T+1, steps)], q
+    u = math.exp(sigma*math.sqrt(delta/steps))# Delta = 1 year/n = number of steps for each year
+    d = math.exp(-sigma*math.sqrt(delta/steps))
+    q = (1 - d) / (u - d)
+    tree = np.zeros((int(steps*T/delta + 1), int(steps*T/delta + 1)))
+    tree[0][0] = S0
+    for i in range(1, int(steps*T/delta) + 1):
+        for j in range(i + 1):
+            tree[j][i] = S0 * (u ** (i - j)) * (d ** j)
+    return tree[:, range(steps, steps * T + 1, steps)], q
 
 
 def priceCliquet(S0, disc, tree, n, q, rec, SurProbFun, datesInYears):
     # Survival probabilities for the expires in datesInYears
     survProb = np.array([SurProbFun(T) for T in datesInYears])
     defProb = np.array([(SurProbFun(T-1)-SurProbFun(T))*rec for T in datesInYears])
-    T = datesInYears[len(datesInYears)-1]
+    T = len(datesInYears)
     payoff = np.zeros(tree.shape)
     for i in range(T):
         if i == 0:
-            for j in range(n):
-                payoff[j, i] += np.max([tree[j, i] - S0, 0]) * bincoeff(n, j) * (q ** (n - j)) * ((1 - q) ** j)
+            for j in range(n + 1):
+                payoff[j, i] += (tree[j, i] - S0) * float((tree[j, i] > S0)) * bincoeff(n, j) * (q ** (n - j)) * ((1 - q) ** j)
         else:
-            for j in range(i * n):
-                for k in range(j, j + n):
-                    payoff[k, i] += np.max([tree[k, i] - tree[j, i - 1], 0]) * bincoeff((i + 1) * n, k) * (q ** ((i + 1) * n - k)) * ((1 - q) ** k)
+            for j in range(i * n + 1):
+                for k in range(j, j + n + 1):
+                    payoff[k, i] += (tree[k, i] - tree[j, i - 1]) * float((tree[k, i] > tree[j, i - 1])) * (bincoeff((i + 1) * n, k) / bincoeff(i * n, j)) * (q ** ((i + 1) * n - k)) * ((1 - q) ** k)
     price = payoff * disc * (survProb + defProb)
+    price = sum(sum(price))
+    return price
+
+
+def priceCliquetBS(S0, disc, tree, n, q, sigma, rec, SurProbFun, datesInYears):
+    # Survival probabilities for the expires in datesInYears
+    survProb = np.array([SurProbFun(T) for T in datesInYears])
+    defProb = np.array([(SurProbFun(T-1)-SurProbFun(T))*rec for T in datesInYears])
+    T = len(datesInYears)
+    payoff = np.zeros(tree.shape)
+    payoff[0, 0] = BS_CALL(S0, S0, datesInYears[0], - np.log(disc[1])/datesInYears[1], 0, sigma)
+    for i in range(1, T):
+        for j in range(i * n + 1):
+            TTM = datesInYears[i] - datesInYears[i - 1]
+            payoff[j, i] = BS_CALL(tree[j, i-1], tree[j, i-1], TTM, - np.log(disc[i + 1]/disc[i])/TTM, 0, sigma) * bincoeff(i * n, j) * (q ** (i * n - j)) * ((1 - q) ** j)
+    price = payoff * disc[0: len(disc) - 1] * (survProb + defProb)
     price = sum(sum(price))
     return price
 
@@ -39,8 +54,18 @@ def bincoeff(n, k):
     if k == 0 or k == n:
         coeff = 1
     else:
-        a = math.factorial(n)
-        b = math.factorial(k)
-        coeff = a // (b * (n - k))
+        if k > n:
+            coeff = 0
+        else:
+            a = math.factorial(n)
+            b = math.factorial(k)
+            c = math.factorial(n - k)
+            coeff = a / (b * c)
     return coeff
 
+
+def BS_CALL(S, K, T, r, d, sigma):
+    # B&S formula for a put option
+    d1 = (np.log(S / K) + (r - d + sigma ** 2 / 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    return S * np.exp(-d * T) * st.norm.cdf(d1) - K * np.exp(-r * T) * st.norm.cdf(d2)
--- provatree.py
+++ provatree.py
@@ -3,17 +3,26 @@ import pandas as pd
 import math
 import option as opt
 
-sigma = math.sqrt(0.25)
-steps = 24
-S0 = 1
+sigma = 0.25
+steps = 34
+S0 = 2.41
 delta = 1
 T = 4
+rec = 0.4
 tree, q = opt.tree_gen(sigma, steps, S0, delta, T)
 
 
 def SurProbFun(t):
-    return np.exp(- 4 * t * 10 ** (-4))
+    intensities = [0.00500001041670575, 0.00635686394536413, 0.00722384773356583, 0.00756985690474914, 0.00739211576159149, 0.00692374267316075, 0.00640219978998205]
+    prob = 1
+    for i in range(len(intensities)):
+        prob = prob * np.exp(- np.max([np.min([t - i, 1]), 0]) * intensities[i])
+    return prob
 
 
-price = opt.priceCliquet(S0, [0.94, 0.9, 0.88, 0.87], tree, steps, q, 0.3, SurProbFun, [1, 2, 3, 4])
+price = opt.priceCliquet(S0, [0.968072448, 0.93889066, 0.914635392, 0.89163513], tree, steps, q, 0.4, SurProbFun, [1, 2.0054794521, 3.0054794521, 4.0054794521])
 print(price)
+priceBS = opt.priceCliquetBS(S0, [1, 0.968072448, 0.93889066, 0.914635392, 0.89163513], tree, steps, q, sigma, 0.4, SurProbFun, [1, 2.0054794521, 3.0054794521, 4.0054794521])
+print(priceBS)
+print(50 * (priceBS - price) * 10 ** 6)
+

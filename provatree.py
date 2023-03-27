7291bdbed87119ced24b5d5352c509aca6419543
Virgi7 <128376370+Virgi7@users.noreply.github.com>
2023-03-27 13:46:47 +0200

    s

--- provatree.py
+++ provatree.py
@@ -17,21 +17,12 @@ T=4
 tree1=opt.tree_gen(sigma, steps, S0, delta,T)
 
 disc = pd.read_excel('dat_disc.xlsx')
-disc.to_numpy()
-print(disc)
+disc=disc.to_numpy()
+print(disc[:,1])
 
+#Ho messo dentro all'excel solo i discounts che ci interessano e
+#con questo comando li leggo e li trasformo in un numpy
 
-dates=['2025-02-02','2026-02-02','2027-02-02','2028-02-02']
-#sett_date=datetime.strptime(sett_date,"%Y-%m-%d")
-#print(sett_date)
-
-
-
-sett_date_y=np.zeros((4,1))
-for i in range(len(dates)):
-    dates[i] = datetime.strptime(dates[i], "%Y-%m-%d")
-    sett_date_y[i]=date.toordinal(dates[i])
-print(sett_date_y)
 
 
 

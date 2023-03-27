9d35e6297dc90b498f647e975925e3b9a771b572
Virgi7 <128376370+Virgi7@users.noreply.github.com>
2023-03-27 17:07:03 +0200

    l

--- provatree.py
+++ provatree.py
@@ -5,25 +5,22 @@ import option as opt
 from datetime import datetime
 from datetime import date
 
+sigma = 0.25
+steps = 34
+S0 = 2.41
+delta = 1
+T = 4
+rec = 0.4
+tree= opt.tree_gen(sigma, steps, S0, delta, T)
 
 
+df = pd.read_excel('dat_disc.xlsx')
+df=df.to_numpy()
+SurvProbs=df[:,3]
+priceBS = opt.priceCliquetBS(S0, df[:,2], tree, steps, sigma, rec, SurvProbs, df[1:,1])
+print(priceBS)
 
 
-sigma=math.sqrt(0.25)
-steps=3
-S0=1
-delta=1
-T=4
-tree1=opt.tree_gen(sigma, steps, S0, delta,T)
-
-disc = pd.read_excel('dat_disc.xlsx')
-disc=disc.to_numpy()
-print(disc[:,1])
-
-
-
-#Ho messo dentro all'excel solo i discounts che ci interessano e
-#con questo comando li leggo e li trasformo in un numpy
 
 
 
